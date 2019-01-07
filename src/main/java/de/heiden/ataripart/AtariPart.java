b3882c64a77549044df75ec2d5d188518696c4b8
Markus Heiden <markus@markusheiden.de>
2019-01-07 22:47:52 +0100

    Migrate to picocli

--- src/main/java/de/heiden/ataripart/AtariPart.java
+++ src/main/java/de/heiden/ataripart/AtariPart.java
@@ -1,168 +1,113 @@
 package de.heiden.ataripart;
 
-import com.beust.jcommander.JCommander;
-import com.beust.jcommander.Parameter;
-import com.beust.jcommander.ParameterException;
-import com.beust.jcommander.Parameters;
 import de.heiden.ataripart.commands.AnalyzeImage;
 import de.heiden.ataripart.commands.ExtractFiles;
 import de.heiden.ataripart.commands.ExtractPartitions;
 import de.heiden.ataripart.commands.ListPartitions;
+import picocli.CommandLine;
+import picocli.CommandLine.*;
 
 import java.io.File;
-import java.io.IOException;
-import java.util.List;
+import java.util.concurrent.Callable;
 
 /**
  * Atari partition analyzer.
  */
-public class AtariPart {
+@Command(name = "ataripart",
+        description = "Atari partition analyzer",
+        mixinStandardHelpOptions = true, usageHelpWidth = 120, version = "1.0.0",
+        subcommands = {
+        HelpCommand.class,
+        AtariPart.AnalyzeCommand.class,
+        AtariPart.ListCommand.class,
+        AtariPart.PartitionsCommand.class,
+        AtariPart.FilesCommand.class
+})
+public class AtariPart implements Runnable {
     /**
      * Start this tool.
      *
      * @param args args[0] has to hold the hard disk image file
      */
     public static void main(String[] args) {
-        HelpOption help = new HelpOption();
-        AnalyzeCommand analyze = new AnalyzeCommand();
-        ListCommand list = new ListCommand();
-        PartitionsCommand partitions = new PartitionsCommand();
-        FilesCommand files = new FilesCommand();
-
-        JCommander commander = JCommander
-                .newBuilder()
-                .programName(AtariPart.class.getSimpleName())
-                .addObject(help)
-                .columnSize(120)
-                .addCommand(analyze)
-                .addCommand(list)
-                .addCommand(partitions)
-                .addCommand(files)
-                .build();
-
         try {
-            commander.parse(args);
+            CommandLine cl = new CommandLine(new AtariPart());
+            cl.parseWithHandler(new RunFirst(), args);
 
-            if (help.isHelp()) {
-                help.execute(commander);
-                return;
-            }
-
-            switch (commander.getParsedCommand()) {
-                case "analyze":
-                    analyze.execute();
-                    return;
-                case "list":
-                    list.execute();
-                    return;
-                case "partitions":
-                    partitions.execute();
-                    return;
-                case "files":
-                    files.execute();
-                    return;
-            }
-        } catch (ParameterException | NullPointerException e) {
-            System.err.println(e.getLocalizedMessage());
-            help.execute(commander);
+        } catch (ExecutionException e) {
+            System.err.println(e.getCause().getLocalizedMessage());
             System.exit(-1);
-            return;
-        } catch (Exception e) {
-            System.err.println(e.getLocalizedMessage());
-            System.exit(-1);
-            return;
         }
     }
 
-    public static class HelpOption {
-        @Parameter(names = {"--help"}, description = "Display help")
-        public Boolean help;
-
-        public boolean isHelp() {
-            return help != null && help;
-        }
-
-        public void execute(JCommander commander) {
-            String command = commander.getParsedCommand();
-            if (command != null) {
-                commander.usage(command);
-            } else {
-                commander.usage();
-            }
-        }
+    @Override
+    public void run() {
+        // Nothing to do.
     }
 
-    @Parameters(commandNames = "analyze", commandDescription = "Search a whole hard disk image for root sectors.")
-    public static class AnalyzeCommand {
-        @Parameter(description = "[Hard disk image]")
-        public List<File> files;
+    @Command(name = "analyze", description = "Search a whole hard disk image for root sectors.")
+    public static class AnalyzeCommand implements Callable<Void> {
+        @Parameters(arity = "1", paramLabel = "image", description = "Hard disk image")
+        private File image;
 
-        public void execute() throws IOException {
-            if (files.isEmpty()) {
-                throw new ParameterException("No hard disk image specified");
-            }
-
-            File image = files.get(0);
+        @Override
+        public Void call() throws Exception {
             new AnalyzeImage().analyze(image);
+            return null;
         }
     }
 
-    @Parameters(commandNames = "list", commandDescription = "List all root sectors and their partitions, starting with the MBR.")
-    public static class ListCommand {
-        @Parameter(names = {"-b", "--backup"}, description = "Display backup root sectors, if any")
-        private Boolean backup = false;
+    @Command(name = "list", description = "List all root sectors and their partitions, starting with the MBR.")
+    public static class ListCommand implements Callable<Void> {
+        @Option(names = {"-b", "--backup"}, description = "Display backup root sectors, if any")
+        private boolean backup = false;
 
-        @Parameter(description = "[Hard disk image]")
-        private List<File> files;
+        @Parameters(arity = "1", paramLabel = "image", description = "Hard disk image")
+        private File image;
 
-        public void execute() throws IOException {
-            if (files.isEmpty()) {
-                throw new ParameterException("No hard disk image specified");
-            }
-
-            File image = files.get(0);
-            new ListPartitions().list(image, backup != null && backup);
+        @Override
+        public Void call() throws Exception {
+            new ListPartitions().list(image, backup);
+            return null;
         }
     }
 
-    @Parameters(commandNames = "partitions", commandDescription = "Extract all partitions to a directory.")
-    public static class PartitionsCommand {
-        @Parameter(names = {"-c", "--convert"}, description = "Convert boot sectors to MS DOS format")
-        public Boolean convertBootSectors = false;
+    @Command(name = "partitions", description = "Extract all partitions to a directory.")
+    public static class PartitionsCommand implements Callable<Void> {
+        @Option(names = {"-c", "--convert"}, description = "Convert boot sectors to MS DOS format")
+        private boolean convertBootSectors = false;
 
-        @Parameter(description = "[Hard disk image] [Directory to copy partition contents to]")
-        public List<File> files;
+        @Parameters(index = "0", paramLabel = "image", description = "Hard disk image")
+        private File image;
+
+        @Parameters(index = "1", paramLabel = "destination", description = "Directory to copy partition contents to", defaultValue = "./atari")
+        private File destinationDir;
 
         /**
          * Extract all partitions of the hard disk image to a directory.
          */
-        public void execute() throws Exception {
-            if (files.isEmpty()) {
-                throw new ParameterException("No hard disk image specified");
-            }
-
-            File image = files.get(0);
-            File destinationDir = files.size() >= 2 ? files.get(1) : new File("./atari");
-            new ExtractPartitions().extract(image, convertBootSectors != null && convertBootSectors, destinationDir);
+        @Override
+        public Void call() throws Exception {
+            new ExtractPartitions().extract(image, convertBootSectors, destinationDir);
+            return null;
         }
     }
 
-    @Parameters(commandNames = "files", commandDescription = "Extract all files from all partitions to a directory. Needs mtools installed.")
-    public static class FilesCommand {
-        @Parameter(description = "[Hard disk image] [Directory to copy files to]")
-        public List<File> files;
+    @Command(name = "files", description = "Extract all files from all partitions to a directory. Needs mtools installed.")
+    public static class FilesCommand implements Callable<Void> {
+        @Parameters(index = "0", paramLabel = "image", description = "Hard disk image")
+        private File image;
+
+        @Parameters(index = "1", paramLabel = "destination", description = "Directory to copy files to", defaultValue = "./atari")
+        private File destinationDir;
 
         /**
          * Copy all files from all partitions of the hard disk image to a directory.
          */
-        public void execute() throws Exception {
-            if (files.isEmpty()) {
-                throw new ParameterException("No hard disk image specified");
-            }
-
-            File image = files.get(0);
-            File destinationDir = files.size() >= 2 ? files.get(1) : new File("./atari");
+        @Override
+        public Void call() throws Exception {
             new ExtractFiles().extract(image, destinationDir);
+            return null;
         }
     }
 }
